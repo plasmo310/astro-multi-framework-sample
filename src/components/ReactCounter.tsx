529b6f20e1b69bc21b1b7eebe2531d4e4f4b0e46
plasmo310 <plasmo310@gmail.com>
2026-02-01 17:59:54 +0900

    add vue component

--- src/components/ReactCounter.tsx
+++ src/components/ReactCounter.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 function ReactCounter() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState<number>(0);
 
   return (
     <div>
---
+++ src/components/VueCounter.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { ref } from "vue";
+
+const count = ref<number>(0);
+
+const increment = (): void => {
+  count.value += 1;
+};
+</script>
+
+<template>
+  <div>
+    <p>Count: {{ count }}</p>
+    <button type="button" @click="increment">Add</button>
+  </div>
+</template>
